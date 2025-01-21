e71b795db60cd41f67401d1e2ca3796852bb0160
MichelleRenyx <yuxinr1@student.unimelb.edu.au>
2025-01-21 19:44:18 +0800

    fix url.py

--- backend/fileprocessor/backend/urls.py
+++ backend/fileprocessor/backend/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += [re_path(r'^(?:.*)/?$)', index, name = 'index')]
+# urlpatterns += [re_path(r'^(?:.*)/?$)', index, name = 'index')]
+urlpatterns += [re_path(r'^(?:.*)/?$', index, name='index')]

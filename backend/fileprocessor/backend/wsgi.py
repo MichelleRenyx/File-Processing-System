2e245a48018d6ea4feb2d69696e4cda262395ef5
MichelleRenyx <yuxinr1@student.unimelb.edu.au>
2025-01-21 19:13:17 +0800

    change setting path in wsgi

--- backend/fileprocessor/backend/wsgi.py
+++ backend/fileprocessor/backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.fileprocessor.backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fileprocessor.backend.settings')
 
 application = get_wsgi_application()
